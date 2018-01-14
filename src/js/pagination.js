1cd8497979489f8a7e9998d14e40788512b838f3
EmixMaxime <emixmaxime@outlook.fr>
2018-01-14 10:22:46 +0100

    (WIP) pagination function

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,17 +1,21 @@
 const sheets = Array.from(document.querySelectorAll('.sheet'))
 const startPaginate = document.querySelector('[data-pagination-start]')
 
-const toPaginate = sheets.slice(sheets.indexOf(startPaginate), sheets.length)
-const paginationStartPage = 2
-
-const beforeText = 'Page n°'
-const afterText = ' / ' + (toPaginate.length + paginationStartPage)
-
-
-for (let i = 1; i <= toPaginate.length; i++) {
-  const paginateEl = document.createElement('div')
-  paginateEl.classList.add('pagination')
-  paginateEl.innerText = beforeText + (i + paginationStartPage) + afterText
-
-  toPaginate[i-1].appendChild(paginateEl)
+const pagination = (startPaginate, sheets) => {
+  const toPaginate = sheets.slice(sheets.indexOf(startPaginate), sheets.length)
+  const paginationStartPage = 2
+  
+  const beforeText = 'Page n°'
+  const afterText = ' / ' + (toPaginate.length + paginationStartPage)
+  
+  
+  for (let i = 1; i <= toPaginate.length; i++) {
+    const paginateEl = document.createElement('div')
+    paginateEl.classList.add('pagination')
+    paginateEl.innerText = beforeText + (i + paginationStartPage) + afterText
+  
+    toPaginate[i-1].appendChild(paginateEl)
+  }
 }
+
+pagination(startPaginate, sheets)
